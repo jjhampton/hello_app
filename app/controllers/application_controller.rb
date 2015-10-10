d52deaba35457ab0889301d6525e8e58b34693ac
John Hampton <john_j_hampton@yahoo.com>
2015-10-10 17:41:14 -0400

    Change content of hello action Add goodbye action and set root route to goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello, world!"
+    render text: "hallo, Welt!"
+  end
+
+  def goodbye
+    render text: "aufwiedersehen, Freund!"
   end
 end
